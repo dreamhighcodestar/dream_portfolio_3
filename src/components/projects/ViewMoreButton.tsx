23c942d39ba3aa7f70b0bf0c2179acb1ea9c542e
dreamhighcodestar <asdsdduazusd@gmail.com>
2025-05-28 17:49:13 +0300

    commit update

--- src/components/projects/ViewMoreButton.tsx
+++ src/components/projects/ViewMoreButton.tsx
@@ -6,7 +6,7 @@ const ViewMoreButton: React.FC = () => {
   return (
     <div className="mt-12 flex justify-center">
       <a 
-        href="https://github.com/dreamhighcodestar" 
+        href="" 
         target="_blank" 
         rel="noopener noreferrer"
         className="flex items-center space-x-2 bg-blue-600/80 hover:bg-blue-600 text-white px-6 py-3 rounded-md transition-colors"
